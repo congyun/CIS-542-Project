60e92f7b84a6e2a9bd50b8b25dcb2e425af715bf
Siyin Gu <gusiyin@gmail.com>
2012-04-18 15:34:44 -0400

    add new files

---
+++ CIS542Project/src/edu/upenn/cis542/utilities/AppConstants.java
@@ -0,0 +1,15 @@
+package edu.upenn.cis542.utilities;
+
+public class AppConstants {
+    public static final String PREFS_NAME = "GPSPrefs";
+    
+    public static final String DEFAULT_TRAVEL_MODE_KEY = "travel_mode";
+    public static final String DEFAULT_TRAVEL_MODE_INITIAL_VALUE = "Walking";
+    
+    public static final String DEFAULT_INTEREST_TYPE_KEY = "interest_type";
+    public static final String DEFAULT_INTEREST_TYPE_INITIAL_VALUE = "Food";
+    
+    public static final String DEFAULT_ALERT_DISTANCE_KEY = "alert_distance";
+    public static final int DEFAULT_ALERT_DISTANCE_INITIAL_VALUE = 1;
+    
+}
