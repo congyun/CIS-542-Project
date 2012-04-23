821869ec4da8bea61e7845a39f577f230c251c78
Siyin Gu <gusiyin@gmail.com>
2012-04-22 22:09:10 -0400

    Make Point Serializable

--- CIS542Project/src/edu/upenn/cis542/route/Point.java
+++ CIS542Project/src/edu/upenn/cis542/route/Point.java
@@ -1,10 +1,14 @@
 package edu.upenn.cis542.route;
 
-public class Point {
-        String mName;
-        String mDescription;
-        String mIconUrl;
-        double mLatitude;
-        double mLongitude;
+import java.io.Serializable;
+
+public class Point implements Serializable {
+    private static final long serialVersionUID = 4610178026065000395L;
+    
+    String mName;
+    String mDescription;
+    String mIconUrl;
+    double mLatitude;
+    double mLongitude;
 }
 
