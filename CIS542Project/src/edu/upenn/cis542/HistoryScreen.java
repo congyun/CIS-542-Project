92c5be7092db0e13ec6d22c09946b7198fb1cc1d
Yufei Wang <yufeiw@seas.upenn.edu>
2012-04-25 02:05:44 -0400

    history routeInfo display

--- CIS542Project/src/edu/upenn/cis542/HistoryScreen.java
+++ CIS542Project/src/edu/upenn/cis542/HistoryScreen.java
@@ -4,7 +4,6 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Iterator;
 import java.util.List;
 
 import com.google.android.maps.GeoPoint;
@@ -14,21 +13,19 @@ import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 
 import edu.upenn.cis542.route.MapOverlay;
-import edu.upenn.cis542.route.PlacesList;
 import edu.upenn.cis542.route.Point;
 import edu.upenn.cis542.route.Road;
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.DialogInterface.OnMultiChoiceClickListener;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class HistoryScreen extends MapActivity {
@@ -36,17 +33,16 @@ public class HistoryScreen extends MapActivity {
 	protected CharSequence[] _options;
 	protected boolean[] _selections;
 	List<Road> historyRoadList;
+	protected int[] selectedIndex = { 0, 0, 0, 0, 0 };
 
 	// mapView Variables
 	private MapView mapView;
 	private MapController mapController;
-	//private Road mRoad;
-	//private PlacesList mList;
 	private Drawable s_marker;
 	private Drawable d_marker;
-	// private Drawable i_marker;
-	//private double fromLat, fromLon, toLat, toLon;
 	List<Overlay> listOfOverlays;
+	int colors[] = { Color.BLUE, Color.GRAY, Color.RED, Color.CYAN,
+			Color.GREEN, Color.YELLOW };
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -75,7 +71,7 @@ public class HistoryScreen extends MapActivity {
 			} while (cursor.moveToNext());
 		}
 		cursor.close();
-		Log.d("cursor:",String.valueOf(cursor.isClosed()));
+		Log.d("cursor:", String.valueOf(cursor.isClosed()));
 		dbHelper.close();
 
 		// if list not empty ?need to determine?
@@ -99,7 +95,7 @@ public class HistoryScreen extends MapActivity {
 		mapView.setSatellite(false);
 		mapController = mapView.getController();
 		mapController.setZoom(13);
-		
+
 		listOfOverlays = mapView.getOverlays();
 		listOfOverlays.clear();
 
@@ -111,7 +107,8 @@ public class HistoryScreen extends MapActivity {
 				.setTitle("Travel Record, please select:")
 				.setMultiChoiceItems(_options, _selections,
 						new DialogSelectionClickHandler())
-				.setPositiveButton("Show Selected", new DialogButtonClickHandler())
+				.setPositiveButton("Show Selected",
+						new DialogButtonClickHandler())
 				.setNegativeButton("Delete Selected",
 						new DialogButtonClickHandler()).create();
 	}
@@ -132,15 +129,17 @@ public class HistoryScreen extends MapActivity {
 			switch (clicked) {
 			case DialogInterface.BUTTON_POSITIVE:
 				int selectedRouteCount = 0;
-				for(int i=0; i<_options.length; i++){
-					if(_selections[i] == true)
+				for (int i = 0; i < _options.length; i++) {
+					if (_selections[i] == true) {
 						selectedRouteCount++;
+					}
 				}
-				if(selectedRouteCount > 5){
+				if (selectedRouteCount > 5) {
 					Context context = getApplicationContext();
-					Toast.makeText(context,"Please select no more than 5 routes to display!", Toast.LENGTH_LONG).show();
-				}
-				else
+					Toast.makeText(context,
+							"Please select no more than 5 routes to display!",
+							Toast.LENGTH_LONG).show();
+				} else
 					printSelectedRoads();
 				break;
 			case DialogInterface.BUTTON_NEGATIVE:
@@ -150,25 +149,58 @@ public class HistoryScreen extends MapActivity {
 		}
 	}
 
+	private int findButtonId(int id) {
+		int buttonId;
+		switch (id) {
+		case 1:
+			buttonId = R.id.route1Button;
+			break;
+		case 2:
+			buttonId = R.id.route2Button;
+			break;
+		case 3:
+			buttonId = R.id.route3Button;
+			break;
+		case 4:
+			buttonId = R.id.route4Button;
+			break;
+		case 5:
+		default:
+			buttonId = R.id.route5Button;
+			break;
+		}
+
+		return buttonId;
+	}
+
 	protected void printSelectedRoads() {
 		int routeCount = 0;
 		for (int i = 0; i < _options.length; i++) {
 			Log.i("ME", _options[i] + " selected: " + _selections[i]);
-			
+
 			if (_selections[i] == true) {
 				Road road = (Road) historyRoadList.get(i);
 				double fromLon = road.mPoints[0].mLongitude;
 				double fromLat = road.mPoints[0].mLatitude;
 				double toLon = road.mPoints[road.mPoints.length - 1].mLongitude;
 				double toLat = road.mPoints[road.mPoints.length - 1].mLatitude;
-				//Log.d("MapPoint", "Points: " + road.mPoints.length);
-				MapOverlay mapOverlay = new MapOverlay(road, mapView, s_marker, d_marker, fromLat, fromLon, toLat, toLon,routeCount);
+				// Log.d("MapPoint", "Points: " + road.mPoints.length);
+				MapOverlay mapOverlay = new MapOverlay(road, mapView, s_marker,
+						d_marker, fromLat, fromLon, toLat, toLon, routeCount);
 				listOfOverlays.add(mapOverlay);
 				Log.d("MapRoute", "Added road size: " + road.mPoints.length);
+
+				// show buttons
+				selectedIndex[routeCount] = i;
+				int buttonId = findButtonId(routeCount + 1);
+				Button button = (Button) findViewById(buttonId);
+				button.setVisibility(0);
+				button.setBackgroundColor(colors[routeCount]);
+
 				routeCount++;
 			}
 		}
-		
+
 		mapView.invalidate();
 
 	}
@@ -188,6 +220,82 @@ public class HistoryScreen extends MapActivity {
 
 	public void onBackToMainButtonClick(View view) {
 		finish();
+	}
+
+	public void onroute1ButtonClick(View view) {
+		Road road = (Road) historyRoadList.get(selectedIndex[0]);
+		routeInfoOnButtonClick(road);
+	}
+
+	public void onroute2ButtonClick(View view) {
+		Road road = (Road) historyRoadList.get(selectedIndex[1]);
+		routeInfoOnButtonClick(road);
+	}
+
+	public void onroute3ButtonClick(View view) {
+		Road road = (Road) historyRoadList.get(selectedIndex[2]);
+		routeInfoOnButtonClick(road);
+	}
+
+	public void onroute4ButtonClick(View view) {
+		Road road = (Road) historyRoadList.get(selectedIndex[3]);
+		routeInfoOnButtonClick(road);
+	}
+
+	public void onroute5ButtonClick(View view) {
+		Road road = (Road) historyRoadList.get(selectedIndex[4]);
+		routeInfoOnButtonClick(road);
+	}
+
+	public void routeInfoOnButtonClick(Road road) {
+
+		String roadInfo;
+
+		DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy h:mm a");
+		Calendar calendar = Calendar.getInstance();
+
+		calendar.setTimeInMillis(road.mStartTime);
+		roadInfo = "Start Time: " + formatter.format(calendar.getTime()) + "\n";
+
+		roadInfo = roadInfo + "Start From: "
+				+ String.format("%.2f", road.mPoints[0].mLatitude) + ","
+				+ String.format("%.2f", road.mPoints[0].mLongitude) + "\n"
+				+ road.mStartName + "\n";
+
+		int end = road.mPoints.length - 1;
+		calendar.setTimeInMillis(road.mEndTime);
+		roadInfo = roadInfo + "End Time: "
+				+ formatter.format(calendar.getTime()) + "\n";
+		roadInfo = roadInfo + "End At: "
+				+ String.format("%.2f", road.mPoints[end].mLatitude) + ","
+				+ String.format("%.2f", road.mPoints[end].mLongitude) + "\n"
+				+ road.mEndName + "\n";
+
+		long elapseTime = road.mEndTime - road.mStartTime;
+		int ss = (int)(elapseTime/1000);
+		int mm = (int)(ss/60);
+		int hh = (int)(mm/60);
+		ss = ss - mm*60;
+		mm = mm - hh*60;
+		
+		roadInfo = roadInfo + "Travel Time: " + String.format("%02d:%02d:%02d", hh,mm,ss) ;
+
+		// zoom to selected route
+		mapView = (MapView) findViewById(R.id.historymapview);
+		int moveToLat = (int) ((road.mPoints[0].mLatitude + road.mPoints[end].mLatitude) * 1000000 / 2);
+		int moveToLong = (int) ((road.mPoints[0].mLongitude + road.mPoints[end].mLongitude) * 1000000 / 2);
+		GeoPoint moveTo = new GeoPoint(moveToLat, moveToLong);
+
+		MapController mapController = mapView.getController();
+		mapController.animateTo(moveTo);
+		mapController.setZoom(15);
+
+		// mapController.zoomToSpan(Math.abs(maxLatitude - minLatitude),
+		// Math.abs(maxLongitude - minLongitude));
+
+		// show route info in Toast
+		Context context = getApplicationContext();
+		Toast.makeText(context, roadInfo, Toast.LENGTH_LONG).show();
 	}
 
 	// create this button! and connect
@@ -228,12 +336,14 @@ public class HistoryScreen extends MapActivity {
 		calendar.setTimeInMillis(road.mStartTime);
 		roadInfo = formatter.format(calendar.getTime()) + "\n"; // "Time:" +
 
-		roadInfo = roadInfo + "From:" + String.format("%.2f",road.mPoints[0].mLatitude) + ","
-				+ String.format("%.2f",road.mPoints[0].mLongitude) + "\n"
+		roadInfo = roadInfo + "From:"
+				+ String.format("%.2f", road.mPoints[0].mLatitude) + ","
+				+ String.format("%.2f", road.mPoints[0].mLongitude) + "\n"
 				+ road.mStartName + "\n";
 		int end = road.mPoints.length - 1;
-		roadInfo = roadInfo + "To:" + String.format("%.2f",road.mPoints[end].mLatitude) + ","
-				+ String.format("%.2f",road.mPoints[end].mLongitude) + "\n"
+		roadInfo = roadInfo + "To:"
+				+ String.format("%.2f", road.mPoints[end].mLatitude) + ","
+				+ String.format("%.2f", road.mPoints[end].mLongitude) + "\n"
 				+ road.mEndName;
 
 		return roadInfo;
